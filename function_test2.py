445b9ff6ec3aaff7c08d083d7bf46894a86e99e9
Edwinna Bikeri <50041140+edwinnab@users.noreply.github.com>
2020-06-19 14:53:22 +0300

    functions test two.

---
+++ function_test2.py
@@ -0,0 +1,10 @@
+#function to add two numbers
+def addition(sum):
+    num1 = int(input("Enter first_number: "))
+    num2 = int(input("Enter second_number: "))
+    sum = num1 + num2
+    return sum 
+
+
+
+print("addition = ", addition(sum))
